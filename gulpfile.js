4b7944fe788a13afd69f200b27dc84e3fa8e5b3b
KalchenkoK <7584663@gmail.com>
2021-06-21 23:08:33 +0300

    2

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      //fileinclude = require('gulp-file-include'),
+      browserSync = require('browser-sync');
+
+gulp.task('sass', function(){
+    return gulp.src("./src/assets/scss/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest('./src/assets/css/'))
+        .pipe(browserSync.stream());
+});
+
+/*gulp.task('fileinclude', function() {
+    return gulp.src('./src/pages/index.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('./'));
+});*/
+
+gulp.task("watch", function(done) {
+    browserSync.init({
+        server:'.'
+    })
+    
+    gulp.watch("./src/assets/scss/**/*.scss", gulp.series('sass'));
+    //gulp.watch("./src/pages/*.html").on('change', gulp.series('fileinclude'));
+    gulp.watch('./index.html').on('change', browserSync.reload);
+    /*gulp.watch('./assets/styles/content.css').on('change', browserSync.reload);
+    gulp.watch('./assets/styles/menu.css').on('change', browserSync.reload);
+    gulp.watch('./assets/styles/mobile.css').on('change', browserSync.reload);
+*/});
+
+gulp.task('default', gulp.series('watch'));
