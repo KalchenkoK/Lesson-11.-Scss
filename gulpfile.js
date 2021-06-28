dfbf1163bf560ac4b7d33962576af65264a9156c
KalchenkoK <7584663@gmail.com>
2021-06-28 16:57:44 +0300

    les14commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 const gulp = require('gulp'),
       sass = require('gulp-sass'),
-      //fileinclude = require('gulp-file-include'),
+      fileinclude = require('gulp-file-include'),
       browserSync = require('browser-sync');
 
 gulp.task('sass', function(){
     return gulp.src("./src/assets/scss/**/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest('./src/assets/css/'))
+        .pipe(gulp.dest('./dist/css/'))
         .pipe(browserSync.stream());
 });
-
+gulp.task('fileinclude', function() {
+    return gulp.src('./src/pages/index.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('./'));
+});
 /*gulp.task('fileinclude', function() {
     return gulp.src('./src/pages/index.html')
         .pipe(fileinclude({
@@ -26,6 +33,7 @@ gulp.task("watch", function(done) {
     
     gulp.watch("./src/assets/scss/**/*.scss", gulp.series('sass'));
     //gulp.watch("./src/pages/*.html").on('change', gulp.series('fileinclude'));
+    gulp.watch("./src/pages/*.html").on('change', gulp.series('fileinclude'));
     gulp.watch('./index.html').on('change', browserSync.reload);
     /*gulp.watch('./assets/styles/content.css').on('change', browserSync.reload);
     gulp.watch('./assets/styles/menu.css').on('change', browserSync.reload);
